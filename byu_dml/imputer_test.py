a4e6828d5c54c4f88fec66e6af40aad1debcd535
Jarom Christensen <jaromchristensen5@gmail.com>
2018-04-06 14:43:18 -0600

    added full imputer primitive code, and a test script for it

---
+++ byu_dml/imputer_test.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+from d3m.container.pandas import DataFrame
+
+from random_sampling_imputer import RandomSamplingImputer
+
+def print_missing_vals_info(df, df_name):
+    num_empty_cols = 0
+    total_num_nan = 0
+    for feature in df:
+        num_nan = np.sum(df[feature].isnull())
+        if num_nan == len(df[feature]):
+            num_empty_cols += 1
+        else:
+            total_num_nan += num_nan
+    print('\nMissing Values for ', df_name)
+    print('\nEmpty columns: ', num_empty_cols)
+    print('Total missing values (not counting empty columns): ', total_num_nan)
+
+if __name__ == '__main__':
+    infile_path = "~/Documents/data/learningData.csv"
+    df = DataFrame(pd.read_csv(infile_path))
+    df.drop("d3mIndex", axis=1, inplace=True)
+    print_missing_vals_info(df, 'Input Dataset')
+    df.to_csv('input.csv', index=False)
+    imputer = RandomSamplingImputer(hyperparams=None, random_seed=0)
+    imputer.set_training_data(inputs=df)
+    imputer.fit()
+    new_df = imputer.produce(inputs=df).value
+    print_missing_vals_info(new_df, 'Imputed Dataset')
+    new_df.to_csv('result.csv', index=False)
---
+++ byu_dml/random_sampling_imputer/random_sampling_imputer.py
@@ -0,0 +1,111 @@
+from d3m.primitive_interfaces.base import CallResult
+from d3m.primitive_interfaces.featurization import FeaturizationPrimitiveBase
+from d3m import metadata
+from d3m.metadata.hyperparams import Hyperparams
+from d3m.metadata.params import Params
+from d3m import container
+import numpy as np
+import pandas
+
+
+__version__ = '0.1.0'
+
+Inputs = container.pandas.DataFrame
+Outputs = container.pandas.DataFrame
+
+class Params(Params):
+    column_vals: container.list.List[pandas.core.series.Series]
+
+class RandomSamplingImputer(FeaturizationPrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
+
+    """
+    A primitive which takes raw data and imputes missing values for each column by randomly sampling from the existing values of that column.
+    If a column has no existing values (aka a completely empty column), the column is ignored and remains in the dataset unimputed"
+    """
+
+    metadata = metadata.base.PrimitiveMetadata({
+        'id': 'ebfeb6f0-e366-4082-b1a7-602fd50acc96',
+        'version': f'v{__version__}',
+        'name': 'Random Sampling Imputer',
+        'source': {
+            'name': 'byu-dml',
+            'contact': 'https://github.com/byu-dml/d3m-primitives'
+        },
+        'installation': [
+            {
+                'type': metadata.base.PrimitiveInstallationType.PIP,
+                'package': 'byudml',
+                'version': str(__version__)
+            }
+        ],
+        'location_uris': [
+            'https://github.com/byu-dml'
+        ],
+        'python_path': 'd3m.primitives.byudml.RandomSamplingImputer',
+        'algorithm_types': [
+            metadata.base.PrimitiveAlgorithmType.IMPUTATION
+        ],
+        'primitive_family': metadata.base.PrimitiveFamily.DATA_PREPROCESSING,
+        'effects': [
+            # not the case if empty columns are just ignored
+            metadata.base.PrimitiveEffects.NO_MISSING_VALUES
+        ]
+    })
+
+    def __init__(self, *, hyperparams: Hyperparams, random_seed: int=0) -> None:
+        super().__init__(hyperparams=hyperparams, random_seed = random_seed)
+        self._column_vals: container.list.List[container.list.List] = None
+        self._random_state = np.random.RandomState(self.random_seed)
+        self._training_inputs: Inputs = None
+        self._fitted: bool = False
+
+    def set_training_data(self, *, inputs: Inputs) -> None:
+        self._training_inputs = inputs
+        self._fitted = False
+
+    def fit(self, *, timeout: float = None, iterations: int = None) -> CallResult[None]:
+        if self._fitted:
+            return CallResult(None)
+
+        if self._training_inputs is None:
+            raise ValueError('Missing training data.')
+
+        self._column_vals = []
+        dataframe = self._training_inputs
+        for feature in dataframe:
+            dropped_nan_series = dataframe[feature].dropna(axis=0, how='any')
+            self._column_vals.append(dropped_nan_series)
+        self._fitted = True
+
+    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
+        if not self._fitted:
+            raise ValueError("Calling produce before fitting.")
+        
+        dataframe = inputs
+        for i in range(len(dataframe.columns)):
+            feature_series = dataframe.iloc[:,i]
+            col = feature_series.as_matrix()
+            num_nan = np.sum(feature_series.isnull())
+            # ignores empty columns
+            if len(self._column_vals[i]) > 0:
+                col[feature_series.isnull()] = self._random_state.choice(self._column_vals[i], num_nan)
+            else:
+                self.logger.warning('WARNING: %s column is completely empty - no values to impute.  This column contains no information and should be dropped by another primitive.', dataframe.columns[i])
+        outputs = dataframe
+
+        outputs.metadata = outputs.metadata.update((), {
+            'schema': metadata.base.CONTAINER_SCHEMA_VERSION,
+            'structural_type': type(outputs)
+        }, source=self)
+
+        return CallResult(outputs)
+
+    def get_params(self) -> Params:
+        return Params(column_vals=self._column_vals)
+
+    def set_params(self, *, params: Params) -> None:
+        self._column_vals = params['column_vals']
+
+
+
+
