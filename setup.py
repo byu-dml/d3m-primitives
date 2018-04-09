8f9e6e1b482543b8fca1bbf031fae3608e838e4d
Jarom Christensen <jaromchristensen5@gmail.com>
2018-04-09 17:29:51 -0600

    setup.py and egg-info added for pip installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from byu_dml.imputer import __version__
+
+setup(
+	name="byudml",
+	packages = find_packages(),
+	version = __version__,
+	description = "A repo of DARPA D3M wrappers for primitives developed by BYU",
+	author = "Roland Laboulaye, Brandom Schoenfeld, Jarom Christensen",
+	url = "https://github.com/byu-dml/d3m-primitives",
+	keywords = ["d3m_primitive"],
+	install_requires = [
+		"numpy",
+		"pandas"
+	],
+	entry_points = {
+    	'd3m.primitives': [
+        	'byudml.imputer.RandomSamplingImputer = byu_dml.imputer:RandomSamplingImputer'
+    	],
+	}
+)
