979479441b4783fe02ea22bb9c6c495bb4032454
Roland Laboulaye <rlaboulaye@gmail.com>
2019-01-28 05:31:43 -0700

    replaced none with default hyperparams

--- byudml/load_d3m_dataset.py
+++ byudml/load_d3m_dataset.py
@@ -2,13 +2,13 @@ from d3m.container.dataset import Dataset
 from d3m.container.pandas import DataFrame
 import os
 print('importing common primitives package...')
-from d3m.primitives.data_transformation.dataset_to_dataframe import Common as DatasetToDataFrame
-from d3m.primitives.data_transformation.column_parser import DataFrameCommon as ColumnParser
+from d3m.primitives.data_transformation.dataset_to_dataframe import Common as DatasetToDataFrame, Hyperparams as DatasetToDataFrameHyperparams
+from d3m.primitives.data_transformation.column_parser import DataFrameCommon as ColumnParser, Hyperparams as ColumnParserHyperparams
 print('done')
 
 def load_dataset():
 	dataset_doc_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', '38_sick_dataset', 'datasetDoc.json'))
 	dataset = Dataset.load(f'file://{dataset_doc_path}')
-	dataframe = DatasetToDataFrame(hyperparams=None).produce(inputs=dataset).value
-	dataframe = ColumnParser(hyperparams=None).produce(inputs=dataframe).value
+	dataframe = DatasetToDataFrame(hyperparams=DatasetToDataFrameHyperparams()).produce(inputs=dataset).value
+	dataframe = ColumnParser(hyperparams=ColumnParserHyperparams()).produce(inputs=dataframe).value
 	return dataframe
