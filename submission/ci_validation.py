7e002193ef34779843cf1f41fcd4b959b618de4d
Orion James Weller <orionw@hal.aml.cs.byu.edu>
2019-06-17 16:49:20 -0600

    Update submodule, add validation

--- submission/ci_validation.py
+++ submission/ci_validation.py
@@ -4,17 +4,16 @@ from byudml import __imputer_path__, __metafeature_path__
 import sys
 sys.path.append(".")
 from submission.utils import get_new_d3m_path
-# change to the primitive repo structure
 
 byu_dir = get_new_d3m_path()
 print("##### Validating json files with D3M Validation #####")
 # validate primitive jsons
 primitive_jsons = Path(byu_dir).glob('**/primitive.json')
-# for filename in primitive_jsons:
-#     # strip the init file path off, since we are changing directories
-#     filename_real = "/".join(str(filename).split("/")[2:])
-#     print("Validating primitive:", filename_real)
-#     os.system("cd submission/primitives/; python3 run_validation.py {}; cd ../..".format(filename_real))  
+for filename in primitive_jsons:
+    # strip the init file path off, since we are changing directories
+    filename_real = "/".join(str(filename).split("/")[2:])
+    print("Validating primitive:", filename_real)
+    os.system("cd submission/primitives/; python3 run_validation.py {}; cd ../..".format(filename_real))  
 
 # check pipelines and meta files
 for filename in Path(byu_dir).glob('**/*.json'):
