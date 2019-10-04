f6de3f4916d8b1713b4338da96b8582213e317f7
root <erik.huckvale2017@gmail.com>
2019-10-04 01:55:19 +0000

    Now testing if the number of columns is either changed or retained depending on imputer hyperparameters

--- tests/test_random_imputer.py
+++ tests/test_random_imputer.py
@@ -1,11 +1,16 @@
 import os
+import pandas as pd
 import unittest
 
 from d3m import index
+from d3m.container.pandas import DataFrame
 
 from byudml.imputer.random_sampling_imputer import RandomSamplingImputer
+from byudml.imputer.random_sampling_imputer import Hyperparams
+from byudml import strings
 
 from tests import utils
+from tests import test_strings
 
 
 DATASETS_DIR = '/datasets/seed_datasets_current'
@@ -27,7 +32,7 @@ class TestRandomSamplingImputer(unittest.TestCase):
     def setUpClass(cls):
         index.register_primitive(RandomSamplingImputer.metadata.query()['python_path'], RandomSamplingImputer)
 
-        cls.classification_dataset_util = utils.D3MDatasetUtil(DATASETS_DIR, '38_sick')
+        cls.classification_dataset_util = utils.D3MDatasetUtil(DATASETS_DIR, test_strings.CLASSIFICATION_DATASET_NAME)
         cls.classification_pipeline_paths = []
         for filename in CLASSIFICATION_PIPELINE_FILENAMES:
             cls.classification_pipeline_paths.append(os.path.join(PIPELINES_DIR, filename))
@@ -54,3 +59,59 @@ class TestRandomSamplingImputer(unittest.TestCase):
                 self.regression_dataset_util.dataset_doc_path,
                 self.regression_dataset_util.problem_path
             )
+
+    def test_column_with_all_unknown_values(self):
+        """
+        Tests the Random Sampling Imputer's ability to handle columns that have all unknown values
+        :return: None
+        """
+
+        data_frame: DataFrame = self._get_test_data_frame()
+        original_cols: list = list(data_frame)
+        leftover_cols: list = list(data_frame)
+        leftover_cols.remove(test_strings.ALL_UNKNOWN_KEY)
+
+        # Test dropping a column with all unknown values
+        output_data_frame: DataFrame = self._get_imputer_output(data_frame, drop_cols_all_unknown_vals=True)
+        output_cols: list = list(output_data_frame)
+        self.assertEqual(output_cols, leftover_cols)
+        self.assertNotEqual(output_cols, original_cols)
+
+        # Test retaining a column with all unknown values
+        output_data_frame = self._get_imputer_output(data_frame, drop_cols_all_unknown_vals=False)
+        output_cols: list = list(output_data_frame)
+        self.assertEqual(output_cols, original_cols)
+        self.assertNotEqual(output_cols, leftover_cols)
+
+    def _get_imputer_output(self, data_frame: DataFrame, drop_cols_all_unknown_vals: bool):
+        imputer = self._get_imputer(data_frame, drop_cols_all_unknown_vals)
+        imputer.fit()
+        input_data_frame = data_frame.copy(deep=True)
+        output_data_frame = imputer.produce(inputs=input_data_frame).value
+        return output_data_frame
+
+    @staticmethod
+    def _get_test_data_frame() -> DataFrame:
+        nan = float('nan')
+        data_frame_dict: dict = {
+            test_strings.ALL_KNOWN_KEY: [1, 2, 3, 3],
+            test_strings.SOME_KNOWN_KEY: [1, nan, 1, 2],
+            test_strings.ALL_UNKNOWN_KEY: [nan, nan, nan, nan]
+        }
+        data_frame: pd.DataFrame = pd.DataFrame(data_frame_dict)
+        data_frame: DataFrame = DataFrame(data_frame, generate_metadata=True)
+        return data_frame
+
+    @staticmethod
+    def _get_imputer_hyperparams(drop_cols_all_unknown_vals: bool) -> Hyperparams:
+        hyperparams: dict = {
+            strings.DROP_COLS_ALL_UNKNOWN_VALS_NAME: drop_cols_all_unknown_vals
+        }
+        hyperparams: Hyperparams = Hyperparams(hyperparams)
+        return hyperparams
+
+    def _get_imputer(self, data_frame: DataFrame, drop_cols_all_unknown_vals: bool) -> RandomSamplingImputer:
+        hyperparams: Hyperparams = self._get_imputer_hyperparams(drop_cols_all_unknown_vals)
+        imputer: RandomSamplingImputer = RandomSamplingImputer(hyperparams=hyperparams)
+        imputer.set_training_data(inputs=data_frame)
+        return imputer
---
+++ tests/test_strings.py
@@ -0,0 +1,4 @@
+ALL_KNOWN_KEY = 'all_known'
+ALL_UNKNOWN_KEY = 'all_unknown'
+SOME_KNOWN_KEY = 'some_known'
+CLASSIFICATION_DATASET_NAME = '38_sick'
