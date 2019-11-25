572b96bc4b11a6fa30f691ce41a8b825c28aac7a
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2019-11-25 15:48:16 -0700

    bump imputer version

--- byudml/__init__.py
+++ byudml/__init__.py
@@ -1,5 +1,5 @@
 __version__ = '0.6.6'
 __metafeature_version__ = '0.4.4'
 __metafeature_path__ = 'd3m.primitives.metalearning.metafeature_extractor.BYU'
-__imputer_version__ = '0.1.4'
+__imputer_version__ = '0.2.0'
 __imputer_path__ = 'd3m.primitives.data_preprocessing.random_sampling_imputer.BYU'
